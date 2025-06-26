4237cd34441599a88197b11a59282514f37bfa68
kath144 <mscatalino2@gmail.com>
2025-06-25 23:14:09 -0500

    react
    help

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <h1>Sabor del Sol</h1>
+      <ul>
+        <li>Inicio</li>
+        <li>Menú</li>
+        <li>Nosotros</li>
+        <li>Contacto</li>
+      </ul>
+    </nav>
+  );
+}
